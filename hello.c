024556e5e2797abd73b77d444ab5598420504edd
Rob Hess <hessro@oregonstate.edu>
2024-10-02 10:54:02 -0700

    Add code from 10/2 lecture.

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int n = 16;
+  float f = 4.75;
+  double d = 4.75757575757575757575;
+  char c = 'a';
+
+  // n = 4.75;
+
+  printf("Hello, world!\n");
+
+  printf("== n: %d, %.2f, %lf, %c\n", n, f, d, c);
+}
