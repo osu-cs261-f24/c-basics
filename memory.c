1a615309a0107d0e4bdc5d469eab341d9fcf34b4
Rob Hess <hessro@oregonstate.edu>
2024-10-14 10:52:16 -0700

    Add code from 10/14 lecture.

--- memory.c
+++ memory.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct student {
   int id;
@@ -39,6 +40,18 @@ struct student* create_student_correctly() {
   return s;
 }
 
+int compare_ints(const void* a, const void* b) {
+  int* a_int = a;
+  int* b_int = b;
+  if (*a_int < *b_int) {
+    return -1;
+  } else if (*a_int > *b_int) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 int main() {
   struct student* s = create_student();
   print_student(s);
@@ -46,4 +59,26 @@ int main() {
   struct student* s2 = create_student_correctly();
   print_student(s2);
   free(s2);
+
+  int* nums = malloc(128 * sizeof(int));  // calloc(128, sizeof(int))
+  for (int i = 0; i < 128; i++) {
+    nums[i] = rand() % 128;
+  }
+  qsort(nums, 128, sizeof(int), compare_ints);
+  printf("\n\n");
+  for (int i = 0; i < 128; i++) {
+    printf("%d  ", nums[i]);
+  }
+  printf("\n\n");
+  free(nums);
+
+  char* str = "cat";
+  // str[1] = 'o';
+  printf("== str: %s\n", str);
+
+  int n = strlen(str);
+  char* copy = malloc((n + 1) * sizeof(char));
+  strncpy(copy, str, n + 1);
+  copy[1] = 'o';
+  printf("== copy: %s\n", copy);
 }
